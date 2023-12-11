f18c5b5991d5134b81754c16f8ea094c3fac48da
Your Name <you@example.com>
2023-12-10 17:22:36 -0800

    evaluation metric

--- func.py
+++ func.py
@@ -21,6 +21,30 @@ def get_user_watch_history(user_id, data, fraction=0.8):
     return titles_list, ratings_list
 
 
+def get_user_watch_history_20(user_id, data, fraction=0.8):
+    """
+    Extracts test title list a specified user's watch history from a given dataset.
+    
+    Args:
+    user_id (int): The ID of the user whose watch history is to be extracted.
+    data (DataFrame): The dataset containing user watch histories.
+    fraction (float, optional): The fraction of the user's watch history to be sampled. Defaults to 0.8.
+
+    Returns:
+    test_titles_list: A list containing just the movie titles that's not in the user's sampled watch history. The rest 20% of the watch history
+    """
+    user_watch_history = data[data['userId'] == user_id]
+    
+    user_history_train = user_watch_history.sample(frac=fraction, random_state=42)
+
+    # Select the rest (e.g., 20%) of the user's history for testing
+    user_history_test = user_watch_history.drop(user_history_train.index)
+    test_title_list = user_history_test['title'].tolist()
+    test_rating = user_history_test['rating'].tolist()
+
+    return test_title_list, test_rating
+
+
 def find_top_3_genre(user_id,data):
     """
     Identifies the top three genres based on average ratings from a specific user's watch history.
@@ -72,3 +96,43 @@ def get_similar_users(user_id, data):
     similar_users = similarity_df[user_id].sort_values(ascending=False)
     most_similar = similar_users.index[1]
     return most_similar
+
+
+def extract_ratings(message):
+    """
+    Extracts movie titles and their ratings from a given text.
+
+    :param message: The string containing the list of movies and ratings.
+    :return: A list of tuples, each containing a movie title and its rating.
+    """
+    import re
+    # Regex pattern to match "Movie Title (Year) - Rating"
+    pattern = r"(\s*[^-]+)\s*-\s*Predicted Rating:\s*(\d+\.\d+)"
+
+    # Find all matches in the text
+    matches = re.findall(pattern, message)
+
+    # Extract movie titles and ratings, converting ratings to float
+    extracted_data = [(match[0], float(match[1])) for match in matches]
+    return extracted_data
+
+
+def calculate_rmse(predicted_ratings, true_ratings):
+    """
+    Calculate the Root Mean Square Error between predicted and true ratings.
+
+    :param predicted_ratings: A list of predicted ratings.
+    :param true_ratings: A list of actual, true ratings.
+    :return: RMSE score.
+    """
+    import math
+    if len(predicted_ratings) != len(true_ratings):
+        raise ValueError("The lengths of predicted ratings and true ratings must be equal.")
+
+    # Calculate the squared differences and their mean
+    squared_differences = [(pred - true) ** 2 for pred, true in zip(predicted_ratings, true_ratings)]
+    mean_squared_difference = sum(squared_differences) / len(squared_differences)
+    
+    # Calculate the square root of the mean squared difference
+    rmse = math.sqrt(mean_squared_difference)
+    return rmse
